11f0d763d964cf0fb9dcac9b002cfd785f179ac3
Jan Linhart <admin@escope.cz>
2015-03-18 11:36:54 +0100

    URL of tracking pixel and form embed are compatible with Mautic 1.0

--- blocks/mautictracker/view.php
+++ blocks/mautictracker/view.php
@@ -1,5 +1,5 @@
 <?php defined('C5_EXECUTE') or die(_("Access Denied.")); ?>
 
 <div id="QuoteBlock<?php   echo intval($bID)?>">
-    <img src="<?php print $mautic_base_url.'/p/mtracking.gif'; ?>" />
+    <img src="<?php print $mautic_base_url.'/mtracking.gif'; ?>" />
 </div>
