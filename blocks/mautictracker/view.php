a235293970206da93949165b0833c0a2b9fba6a5
Takuro Hishikawa <hishikawa@concrete5.co.jp>
2015-11-19 06:51:14 +0900

    Show placeholder instead of tracking pixel in edit mode

--- blocks/mautictracker/view.php
+++ blocks/mautictracker/view.php
@@ -1,3 +1,9 @@
 <?php defined('C5_EXECUTE') or die(_("Access Denied.")); ?>
 
-<img style="display:none" src="<?php  print $mautic_base_url; ?>/mtracking.gif<?php echo $encodedAttrs ?>" />
+if (\Page::getCurrentPage()->isEditMode()) { ?>
+    <div class="ccm-edit-mode-disabled-item">
+        <?php echo t('Mautic Tracker disabled in edit mode.') ?>
+    </div>
+<?php } else { ?>
+    <img style="display:none" src="<?php  print $mautic_base_url; ?>/mtracking.gif<?php echo $encodedAttrs ?>" />
+<?php }
